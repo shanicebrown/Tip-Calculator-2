503685e5b24a97ff448ad032f690d656ed8de564
Shanice Brown <s.mariebrown10@gmail.com>
2021-03-31 07:34:46 -0500

    Initial Commit

---
+++ Tip Calculator 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tip Calculator 2
+//
+//  Created by Shanice Brown (WHA) on 3/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tip Calculator 2/Tip_Calculator_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_Calculator_2App.swift
+//  Tip Calculator 2
+//
+//  Created by Shanice Brown (WHA) on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Tip_Calculator_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
